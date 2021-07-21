1c21f46df9ca5bb992d6880fafb107765f2dc93c
Min Khant Soe <63970830+1MinKhantSoe1@users.noreply.github.com>
2021-07-22 03:56:39 +0800

    Add files via upload

---
+++ Basic_input_and_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float m, cm;
+
+    cout << "Enter the value in centimeters: ";
+    cin >> cm;
+    cout << "The value in meter is: " << cm/100;
+
+    return 0;
+
+}
